33f3bd9399ec283954f17b08fdab6bc8c9d46b81
Nick Mensah <47987675+mensahtribe@users.noreply.github.com>
2020-05-13 15:11:14 -0500

    Add files via upload
    initial project commit please enjoy

---
+++ Mensah-Tribe-Solutions-landing-page-fcc/script.js
@@ -0,0 +1,43 @@
+// Navbar condition
+const navbar = document.getElementById('navbar');
+let scrolled = false;
+
+window.onscroll = function () {
+  if (window.pageYOffset > 100) {
+    navbar.classList.remove('top');
+    if (!scrolled) {
+      navbar.style.transform = "translateY(-70px)";
+    }
+    setTimeout(function () {
+      navbar.style.transform = "translateY(0)";
+      scrolled = true;
+    }, 200);
+  } else {
+    navbar.classList.add('top');
+    scrolled = false;
+  }
+}
+
+// smooth Scrolling
+$('#navbar a, .btn').on('click', function (e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
+// js example
+// const para = document.getElementById('js-example');
+
+// para.addEventListener('click', updateName);
+
+// function updateName() {
+//   let name = prompt('Enter a new name');
+//   para.textContent = 'Player 1: ' + name;
+// }
